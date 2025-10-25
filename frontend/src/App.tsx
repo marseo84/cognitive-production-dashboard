8613c1a7fc28087fbff5e47be290e9aebed24014
Marcel Bosek <himarseo@gmail.com>
2025-10-25 07:02:21 +0200

    tailwind installed

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="h-screen w-screen bg-blue-500 flex items-center justify-center">
+      <h1 className="text-white text-4xl font-bold">Tailwind is working!</h1>
+    </div>
+  );
+}
+
+export default App;
